a76a55c7d61e1cc46919991efe1546773ac9884d
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-04 19:05:56 +0530

    [Shreya]Add: Adding the Employee Wage final solution

---
+++ EmpWageFinalSolution/IComputeEmpWage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWageFinalSolution
+{
+     interface IComputeEmpWage
+    {
+        void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+        void computeEmpWage();
+        int getTotalWage(string company);
+    }
+}
